df82759db4ef2ad25a79b930b87bd8049af74933
Shohei SUNAGAWA <kanpe777@gmail.com>
2014-04-17 10:59:33 +0900

    static_page の test の重複部分を消した
    ```
    $ vim spec/requests/static_pages_spec.rb
    $ bundle exec rspec spec/requests/static_pages_spec.rb
    (成功)
    ```

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -9,7 +11,7 @@ describe "Static pages" do
     end
     it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+      expect(page).to have_title(base_title)
     end
     it "should not have a custom page title" do
       visit '/static_pages/home'
@@ -24,7 +26,7 @@ describe "Static pages" do
     end
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -35,7 +37,7 @@ describe "Static pages" do
     end
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
   end
 
@@ -46,7 +48,7 @@ describe "Static pages" do
     end
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
