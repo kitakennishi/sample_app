66d4c97435dfe89ca7ea9804d65515d39c04640f
Shohei SUNAGAWA <kanpe777@gmail.com>
2014-04-20 12:26:03 +0900

    flashがそのままというバグを確認するテスト記述, サインインのテスト(失敗版と成功版)しかし、失敗版しかちゃんとかけてないので、成功版はとりあえずコメントアウト状態
    ```
    $ spec/requests/authentication_pages_spec.rb
    ```

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -16,21 +16,28 @@ describe "Authentication" do
       before { click_button "Sign in" }
 
       it { should have_title('Sign in') }
-      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
-    end
+      it { should have_selector('div.alert.alert-danger', text: 'Invalid') }
 
-    describe "with valid information" do
-      let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-danger') }
       end
-
-      it { should have_title(user.name) }
-      it { should have_link('Profile', href: user_path(user)) }
-      it { should have_link('Sign out', href: signout_path) }
-      it { should_not have_link('Sign in', href: signin_path) }
     end
+
+#    describe "with valid information" do
+#      let(:user) { FactoryGirl.create(:user) }
+#      before do
+#        fill_in "session_email",    with: user.email.upcase
+#        fill_in "session_password", with: user.password
+#        click_button "Sign in"
+#      end
+#
+#      it { should have_title('Sign in') }
+#      it { should have_selector('div.alert.alert-danger', text: 'Invalid') }
+#      it { should have_title(user.name) }
+#      it { should have_link('Profile', href: user_path(user)) }
+#      it { should have_link('Sign out', href: signout_path) }
+#      it { should_not have_link('Sign in', href: signin_path) }
+#    end
   end
 end
