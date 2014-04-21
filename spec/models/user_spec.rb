1f02a3d00c915900fa0616860a1541db175c0152
Shohei SUNAGAWA <kanpe777@gmail.com>
2014-04-21 16:23:40 +0900

    ユーザのマイクロソフトの順序(新しいものから)
    ```
    $ vim spec/models/user_spec.rb
    $ vim app/models/micropost.rb
    $ bundle exec rspec
    (成功)
    ```

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -119,4 +119,19 @@ describe User do
     its(:remember_token) { should_not be_blank }
     #it { expect(@user.remember_token).not_to be_blank }と等価
   end
+
+  describe "micropost associations" do
+
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+    end
+  end
 end
