1b779157eb32dea27a12a92def939c7f3f40278f
Shohei SUNAGAWA <kanpe777@gmail.com>
2014-04-21 09:36:28 +0900

    演習9.6.3 サインインの有無での[Profile], [Settings]の有無のテスト
    ```
    $ vim spec/requests/authentication_pages_spec.rb
    $ bundle exec rspec
    (成功)
    ```

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,6 +7,8 @@ describe "Authentication" do
 
     it { should have_content('Sign in') }
     it { should have_title('Sign in') }
+    it { should_not have_link('Profile') }
+    it { should_not have_link('Settings') }
   end
 
   describe "signin" do
@@ -17,6 +19,8 @@ describe "Authentication" do
 
       it { should have_title('Sign in') }
       it { should have_error_message('Invalid') }
+      it { should_not have_link('Profile') }
+      it { should_not have_link('Settings') }
 
       describe "after visiting another page" do
         before { click_link "Home" }
